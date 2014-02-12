0a43a2cce967903241dbee96b543a6f146c3c921
Mattey40 <matmattie@gmail.com>
2014-02-12 20:50:52 +0100

    Os for bluetooth robot. ok overwrite de files maar omdat ze toch al anders zijn dan het origineel

--- RobOS-Test-NXTVersion/src/Main.java
+++ RobOS-Test-NXTVersion/src/Main.java
@@ -1,133 +1,60 @@
-import lejos.nxt.Motor;
-import lejos.nxt.Button;
-import lejos.nxt.Sound;
-import lejos.nxt.LCD;
-public class Main {
-	public static BTConnection BTLink;
-	  public static BTConnection btLink;
+import java.io.*;
 
-	public static void main(String[] args) {
-		MDraw G2d = new MDraw();
-		G2d.DrawHorz(10, 10, 100);
-		MMuziek player = new MMuziek();
-		player.IntroMuziek();
-		G2d.FillRect(10, 10, 100, 100);
-		
-		System.out.println("Booting");
-		Button.waitForAnyPress();
-		
-		
-		
+import lejos.nxt.*;
+import lejos.nxt.comm.*;
 
-	}
-	public static void connect()
-	{ 
-	   System.out.println("Listening");
-	   BTLink = Bluetooth.waitForConnection();   
-	   dataOut = BTLink.openDataOutputStream();
-	   dataIn = BTLink.openDataInputStream();
+public class Main 
 
-	}
-	
-	
+{
+  public static void main(String[] args) throws IOException { 
+    LCD.drawString("right BT",0, 0);
+    NXTConnection connection = null;
 
-}
-class MMuziek{
-	public void IntroMuziek() {
-		Sound.beep();
-		Sound.pause(5);
-		Sound.beep();
-		Sound.playNote(Sound.PIANO, 700, 2000);
-		Sound.pause(1000);
-		Sound.playNote(Sound.PIANO, 600, 2000);
-		
-		
-		Sound.playNote(Sound.PIANO, 700, 1000);
-		Sound.playNote(Sound.PIANO, 600, 1000);
-		Sound.playNote(Sound.PIANO, 900, 2000);
-		
-		//couplet een
-		Sound.playNote(Sound.XYLOPHONE, 900, 500);
-		Sound.pause(200);
-		Sound.playNote(Sound.XYLOPHONE, 1000, 500);
-		Sound.pause(200);
-		Sound.playNote(Sound.XYLOPHONE, 1100, 500);
-		Sound.pause(200);
-		Sound.playNote(Sound.XYLOPHONE, 1200, 500);
-		Sound.pause(200);
-		Sound.playNote(Sound.XYLOPHONE, 1100, 500);
-		Sound.pause(200);
-		Sound.playNote(Sound.XYLOPHONE, 1000, 500);
-		Sound.pause(200);
-		Sound.playNote(Sound.XYLOPHONE, 1200, 500);
-		Sound.pause(200);
-		Sound.playNote(Sound.XYLOPHONE, 1100, 1000);
-		Sound.playNote(Sound.XYLOPHONE, 1000, 1000);
-		Sound.playNote(Sound.XYLOPHONE, 900, 1000);
-		Sound.playNote(Sound.XYLOPHONE, 800, 2000);
-		//intro xylo
-		Sound.beep();
-		Sound.pause(5);
-		Sound.beep();
-		Sound.playNote(Sound.XYLOPHONE, 700, 4000);
-		Sound.pause(1000);
-		Sound.playNote(Sound.XYLOPHONE, 600, 4000);
-		
-		
-		Sound.playNote(Sound.XYLOPHONE, 700, 4000);
-		Sound.playNote(Sound.XYLOPHONE, 600, 4000);
-		Sound.playNote(Sound.XYLOPHONE, 900, 4000);
-		
-		
-		/*Sound.playNote(Sound.PIANO, 900, 1000);
-		Sound.playNote(Sound.PIANO, 800, 1000);
-		Sound.playNote(Sound.PIANO, 700, 1000);
-		Sound.playNote(Sound.PIANO, 600, 1000);
-		Sound.playNote(Sound.PIANO, 500, 1000);
-		Sound.playNote(Sound.PIANO, 400, 1000);*/
-		
-		}
-}
-class MDraw{
-	
-	public void DrawHorz(int xbegin,int ybegin, int xeind) {
-		for(int x=0+xbegin; x<xeind-xbegin;x++){
+    if(Button.waitForAnyPress() == Button.ID_RIGHT){
+      LCD.drawString("waiting for BT", 0,1 );
+      connection = Bluetooth.waitForConnection();
+    } else {
+      LCD.drawString("waiting for USB", 0,1 );
+      connection = USB.waitForConnection();
+    }
+    System.out.println("Connection made");
+
+    DataOutputStream Out = connection.openDataOutputStream();
+    DataInputStream In = connection.openDataInputStream();
+    Controller control = new Controller(Out, In);
+  }   
+}//a
+class Controller{
+	private int cmd1,cmd2,cmd3,cmd0;
+	private NXTRegulatedMotor motorL= Motor.A,motorR= Motor.B, motorF = Motor.C;
+	private boolean connected = true;
+	private DataInputStream Input;
+	private DataOutputStream Output;
+	public Controller(DataOutputStream Out,DataInputStream In) throws IOException{
+		System.out.println("Controller Activated");
+		Input = In;
+		Output = Out;
+		while(connected == true){
+			int check = Input.read();
+			System.out.println(check);
+			int Arot = 0;
+			int Brot = 0;
+			motorL.setSpeed(500);
+			motorR.setSpeed(500);
+			motorR.setSpeed(900);
 			
-		LCD.setPixel(x, ybegin, 1); }
-	}
-	public void DrawVert(int xbegin,int ybegin, int xeind) {
-		for(int x=0+xbegin; x<xeind-xbegin;x++){
 			
-		LCD.setPixel(x, ybegin, 1); }
-	}
-	
-	public void FillRect(int xbegin, int ybegin, int hoogte, int Breed){
-		for(int h = 0; h< hoogte; h++){
-
-			int xd = h+xbegin;
-			int yd = h+ybegin;
-			int bd = h+Breed;
+			if (check == 1){Arot = Arot +500;Brot = Brot +500;motorL.rotate(Arot,true);motorR.rotate(Brot,false);}
+			if (check == 2){motorL.setSpeed(700);Arot = Arot +800;Brot = Brot +500;motorL.rotate(Arot,true);motorR.rotate(Brot,false);}
+			if (check == 3){motorR.setSpeed(700);Brot = Brot +800;Arot = Arot +500;motorL.rotate(Arot,true);motorR.rotate(Brot,false);}
+			if (check == 4){Arot = Arot -500;Brot = Brot -500;motorL.rotate(Arot,true);motorR.rotate(Brot,false);}
+			if (check == 5){motorF.rotate(360, false);}
 			
-			DrawHorz(xd,yd,bd);
-		//	System.out.println("test"+h);
-
 			
-		}
-	}
-}
-class Control extends Thread{
-	public void schiet(int aantal){ //testmethode om te kijken of alles goed werkt, en omdat het awesome is...
-		for(int i=0;i==1;i++){
-			Motor.C.forward();
 			
-		}
-	}
-
-	public String s() {
+			
+			
+			
+		}}
 		
-		Motor.C.forward();
-		return "helloworld";
 	}
-}
-
-
